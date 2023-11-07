b82715083b46012d5074f48aec141eb35db7e7af
326206984 <326206984@PC9-18>
2023-11-07 17:38:01 +0200

    Add project files.

---
+++ exercise01/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace exercise01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public Products[] Products  { get; set;}
+
+        // GET: api/Product
+        [HttpGet]
+        public ActionResult<IEnumerable<Products>> GetAllProduct()
+        {
+
+            var orders = new List<Products>
+            {
+                new Products { Name="sdfghjk", Id="5" },
+                new Products { Name="sara", Id="326206984" }
+            };
+            return Ok(orders);
+        }
+        // GET: api/Product/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Products> GetProductId(string id)
+        {
+            var product = new Products
+            {
+                Id = id,
+                Name = $"product {id}"
+            };
+            return Ok(product);
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void CreateOrder([FromBody] Products Product)
+        {
+            // Logic to create order
+        }
+
+        // PUT: api/Product/1
+        [HttpPut("{id}")]
+        public void UpdateOrder( string id, [FromBody] Products Product)
+        {
+            Product.Id = id;
+            // Logic to update order
+        }
+    }
+}
---
+++ exercise01/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace exercise01
+{
+    public class Products
+    {
+        public string Name { get; set; } 
+        public string Id { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+    }
+}
